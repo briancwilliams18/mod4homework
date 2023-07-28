2cd78b22f3a6d5461f0d38139158a0aa2f18eec1
Brian Williams <briancwilliams18@gmail.com>
2023-07-28 12:18:40 -0400

    base html and js questions

---
+++ script.js
@@ -0,0 +1,27 @@
+let quizQuestions = [ 
+{
+    question: "what does 'DOM' stand for?",
+    options: ["Document Object Model", "Data Object Model", "Data Object Manual", "Document Oriented Model"],
+    answer: "Document Object Model"
+},
+{
+    question: "Javascript is what type of language?",
+    options: ["Object-Based", "Procedural", "Object-Oriented", "None of these"],
+    answer: "Object-Oriented"
+},
+{
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    options: ["getElementById()", "getElementsByClassName()", "Both of these", "None of these"],
+    answer: "Both of these"
+},
+{
+    question: "The process in which an object or data structure is translated into a format suitable for transferral over a network, or storage is called?",
+    options: ["Object Serialization", "Object Encapsulation", "Object Inheritance", "None of these"],
+    answer: "Object Serialization"
+},
+{
+    question: "How do you write a comment in javascript?",
+    options: ["/* */", "#", "$ $", "//"],
+    answer: "//"
+}
+]
